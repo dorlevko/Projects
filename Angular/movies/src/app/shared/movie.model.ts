48b6279a326f23fcf1e680bb2fa592058ad5b919
dorlevko <dorlevkovich@gmail.com>
2020-10-16 19:43:46 +0300

    Studies

--- Angular/movies/src/app/shared/movie.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Movie{
-    id: number;
-    movieName: string;
-    price: string;
-    movieDate: string;
-}
--- Website/README.md
+++
@@ -1,3 +0,0 @@
-My weather website.
-
-production on heroku: https://website-project-90825.herokuapp.com/
